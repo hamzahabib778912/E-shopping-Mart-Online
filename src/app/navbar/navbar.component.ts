3021d897fdeda0a6b1fa1d72c767004211ac078e
hamzahabib778912 <hamzahabib778912@gmail.com>
2024-06-05 11:07:34 +0500

    Covers: Added environment related changes to test different env files

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'navbar',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  navBarColor = environment.navBarColor;
   constructor() { }
 
   ngOnInit() {
